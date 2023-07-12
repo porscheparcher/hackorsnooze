9b4e53e4742e56031af303892820253658cbdc7c
Porsche Parcher <130427863+porscheparcher@users.noreply.github.com>
2023-07-12 11:53:23 -0600

    Add files via upload

---
+++ stories.js
@@ -0,0 +1,86 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+/**Handle the form submit when adding a new story */
+
+function formSubmit() {
+  Array.from(document.querySelectorAll('#story-form'))
+  .reduce((acc, input) =>
+  ({...acc, [input.id]: input.value}), {});
+  addStory();
+  
+}
+
+const submitEntry = document.getElementById('add-story');
+submitEntry.addEventListener('click', formSubmit)
+
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    const favoriteStar = document.createElement('span')
+    favoriteStar.className = 'far fa-star';
+    $story.append(favoriteStar);
+    $allStoriesList.append($story);
+
+    favoriteStar.addEventListener('click', function() {
+      event.preventDefault();
+      this.classList.remove('far');
+      this.classList.add('fas');
+    })
+    if (favoriteStar.className === ('fas')) {
+      favoriteStar.removeClass('fas');
+      favoriteStar.addClass('far')
+    
+
+    }
+      
+    
+    
+  }
+
+  $allStoriesList.show();
+}
+
+
+
